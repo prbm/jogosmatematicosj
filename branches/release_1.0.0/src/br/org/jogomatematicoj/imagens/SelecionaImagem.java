c7baaf9632e38eb3a249b13667001ffbd2a9447a
paulo.ricardobm@gmail.com <paulo.ricardobm@gmail.com@e554a57e-7207-da3c-0ca8-2a9d20a36ebb>
2013-11-24 00:38:03 +0000

    BASELINE release version 1.0.0

---
+++ branches/release_1.0.0/src/br/org/jogomatematicoj/imagens/SelecionaImagem.java
@@ -0,0 +1,54 @@
+package br.org.jogomatematicoj.imagens;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Paulo Ricardo
+ */
+public class SelecionaImagem {
+
+        private String[] imagensNumeroPath = new String[10];
+        private String[] imagensOperacaoPath = new String[4];
+        private String imagesRoot = null;
+        private static final String EXTENSION_FILE_PNG = ".png";
+        
+        public SelecionaImagem(){
+            imagensNumeroPath[0]="/000" + EXTENSION_FILE_PNG;
+            imagensNumeroPath[1]="/001" + EXTENSION_FILE_PNG;
+            imagensNumeroPath[2]="/002" + EXTENSION_FILE_PNG;
+            imagensNumeroPath[3]="/003" + EXTENSION_FILE_PNG;
+            imagensNumeroPath[4]="/004" + EXTENSION_FILE_PNG;
+            imagensNumeroPath[5]="/005" + EXTENSION_FILE_PNG;
+            imagensNumeroPath[6]="/006" + EXTENSION_FILE_PNG;
+            imagensNumeroPath[7]="/007" + EXTENSION_FILE_PNG;
+            imagensNumeroPath[8]="/008" + EXTENSION_FILE_PNG;
+            imagensNumeroPath[9]="/009" + EXTENSION_FILE_PNG;
+            
+            imagensOperacaoPath[0]="/soma" + EXTENSION_FILE_PNG;
+            imagensOperacaoPath[1]="/subtracao" + EXTENSION_FILE_PNG;
+            imagensOperacaoPath[2]="/multiplicacao" + EXTENSION_FILE_PNG;
+            imagensOperacaoPath[3]="/divisao" + EXTENSION_FILE_PNG;
+            
+            imagesRoot = "/br/org/jogomatematicoj/imagens";
+        }
+        
+        public ImageIcon getNumeroIcon(int numero){
+            //numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
+            String path =  imagesRoot + imagensNumeroPath[numero];
+            ImageIcon icon = new ImageIcon(getClass().getResource(path));
+            return icon;
+        }
+
+        public ImageIcon getOperacaoIcon(int numero){
+            String path = imagesRoot + imagensOperacaoPath[numero];
+            ImageIcon icon = new ImageIcon(getClass().getResource(path));
+            return icon;
+        }
+
+        public ImageIcon getBlankIcon(){
+            String path = imagesRoot + "/blank" + EXTENSION_FILE_PNG;
+            ImageIcon icon = new ImageIcon(getClass().getResource(path));
+            return icon;
+        }
+}
